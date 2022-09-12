c5e8d1acd27c5436ce0474b454f911bfeed5c8e7
cyj011 <1223752886@qq.com>
2022-09-12 19:11:32 +0800

    Update liangshuhe.java

--- liangshuhe.java
+++ liangshuhe.java
@@ -7,16 +7,17 @@ public class main {
 	}
 }
 
+//学号：202102915
+//姓名：蔡宇杰
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    	int[] sum =new int[2];
-    	for(int i =0;i<nums.length;i++)
-    		for(int j=i+1;j<=nums.length;j++)
-    			if(nums[i]+nums[j]==target ) {
+       int[] sum =new int[2];
+    	for(int i =0;i<=nums.length-2;i++)
+    		for(int j=i+1;j<=nums.length-1;j++)
+    			if(nums[i]+nums[j]== target &&i!=j) {
     				sum[0]=i;
     				sum[1]=j;
     			}
     	return sum;
-       
     }
 }
